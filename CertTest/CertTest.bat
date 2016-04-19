7a2d955cfac89b43d8282df7311b65b81921297d
Greg Hayman <gregory.hayman@nrel.gov>
2016-04-19 14:34:59 +0000

    Updated binaries and CertTest results for v2.05.00 release Added a x64 exe in order to run the last CertTest because it now can run out of memory in 32bit mode.
    git-svn-id: https://windsvn2.nrel.gov/HydroDyn/trunk@671 c0d87ae3-59f3-4fc3-86f6-af0122200c1e

--- CertTest/CertTest.bat
+++ CertTest/CertTest.bat
@@ -18,6 +18,7 @@ rem @SET MBC_SOURCE=C:\Users\bjonkman\Data\DesignCodes\MBC\Source
 
 ::=======================================================================================================
 IF /I "%1"=="-DEBUG" GOTO debugVer
+IF /I "%1"=="-X64" GOTO x64
 IF /I "%1"=="-GFORTRAN" GOTO gfortran
 IF /I "%1"=="-IFORT" GOTO ifort
 IF /I "%1"=="-DEVBUILD" GOTO devBuild
@@ -26,6 +27,11 @@ IF /I "%1"=="-DEVDEBUG" GOTO devDebugBuild
 :releaseVer
 @SET EXE_VER=Using released version of HydroDynDriver (IVF/VS)
 @SET HydroDyn=..\bin\HydroDynDriver_win32.exe
+goto CertTest
+
+:x64
+@SET EXE_VER=Using released version of HydroDynDriver (IVF/VS)
+@SET HydroDyn=..\bin\HydroDynDriver_x64.exe
 goto CertTest
 
 :debugVer
